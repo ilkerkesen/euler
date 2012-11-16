06fff9bc1b1770aa0cd7a836869bfe306fda89e8
Ilker Kesen <ilkerksn@gmail.com>
2012-11-16 09:47:38 +0200

    better without curly-braces

--- source/problem31.c
+++ source/problem31.c
@@ -16,24 +16,16 @@ int main()
 
   sum = 0;
 
-  for(a = 0; a <= LIMIT; a += 200) {
-    for(b = 0; a + b <= LIMIT; b += 100) {
-      for(c = 0; a + b + c <= LIMIT; c += 50) {
-	for(d = 0; a + b + c + d <= LIMIT; d += 20) {
-	  for(e = 0; a + b + c + d + e <= LIMIT; e += 10) {
-	    for(f = 0; a + b + c + d + e + f <= LIMIT; f += 5) {
-	      for(g = 0; a + b + c + d + e + f + g <= LIMIT; g +=2) {
-		for(h = 0; a + b + c + d + e + f + g + h <= LIMIT; h += 1) {
+  for(a = 0; a <= LIMIT; a += 200)
+    for(b = 0; a + b <= LIMIT; b += 100)
+      for(c = 0; a + b + c <= LIMIT; c += 50)
+	for(d = 0; a + b + c + d <= LIMIT; d += 20)
+	  for(e = 0; a + b + c + d + e <= LIMIT; e += 10)
+	    for(f = 0; a + b + c + d + e + f <= LIMIT; f += 5)
+	      for(g = 0; a + b + c + d + e + f + g <= LIMIT; g +=2)
+		for(h = 0; a + b + c + d + e + f + g + h <= LIMIT; h += 1)
 		  if(a + b + c + d + e + f + g + h == LIMIT)
 		    sum++;
-		}
-	      }
-	    }
-	  }
-	}
-      }
-    }
-  }
 
   printf("%d\n", sum);
 
