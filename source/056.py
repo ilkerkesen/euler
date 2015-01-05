0daad34f019f06751b24b5c7e954dee67fedbc89
ilker <ilkerksn@gmail.com>
2015-01-05 09:44:24 +0200

    less is better

--- source/056.py
+++ source/056.py
@@ -4,20 +4,16 @@
 from itertools import product
 
 
-def digit_sum(number):
-    return reduce(lambda x, y: x+y, map(int, str(number)))
-
-
 def main():
     ps = product(range(1, 100), repeat=2)
-    max_sum = 0
+    max_digit_sum = 0
 
     for a, b in ps:
-        c = digit_sum(a**b)
-        if c > max_sum:
-            max_sum = c
+        digit_sum = reduce(lambda x, y: x+y, map(int, str(a**b)))
+        if digit_sum > max_digit_sum:
+            max_digit_sum = digit_sum
 
-    print max_sum
+    print max_digit_sum
 
 
 if __name__ == "__main__":
