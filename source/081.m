ebaa4f6444d1a070cd56d8c21ea3f368827c291f
Ilker Kesen <ilkerksn@gmail.com>
2015-10-03 22:25:32 +0300

    problem 81 added

---
+++ source/081.m
@@ -0,0 +1,19 @@
+load p081_matrix.txt;
+matrix = p081_matrix;
+[m, n] = size(matrix);
+
+for i = 1:m
+    for j = 1:n
+        if (i == 1 && j == 1)
+            continue;
+        elseif (i == 1)
+            matrix(i, j) += matrix(i, j-1);
+        elseif (j == 1)
+            matrix(i, j) += matrix(i-1, j);
+        else
+            matrix(i, j) += min(matrix(i, j-1), matrix(i-1, j));
+        end
+    end
+end
+
+printf("%d\n", matrix(m, n));
